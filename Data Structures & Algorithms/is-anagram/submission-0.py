87079ea8f2fa6195e868bd524e97e6b26c5d2e0a
Alan Khinkis <khinkisalan@gmail.com>
2026-08-27 13:15:12 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        my_dict = {}
+        for char in s:
+            if char in my_dict:
+                my_dict[char] += 1
+            else:
+                my_dict[char] = 1
+        for char in t:
+            if char in my_dict:
+                my_dict[char] -= 1
+            else:
+                return False
+        for char in my_dict:
+            if my_dict[char] != 0:
+                return False
+        return True
